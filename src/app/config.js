2cfe9898603dc9cc2be56294f92e1bfff2555b09
Carlos-iso <carlosbalak2002@gmail.com>
2023-04-11 10:42:09 -0300

    Back enverronment

--- src/app/config.js
+++ src/app/config.js
@@ -4,10 +4,11 @@ const saltKey = process.env.SALT_KEY;
 const mongodbUsername = process.env.MONGODB_USERNAME;
 const mongodbPassword = process.env.MONGODB_PASSWORD;
 const mongodbCluster = process.env.MONGODB_CLUSTER;
+const mongodbNet = process.env.MONGODB_NET;
 const mongodbDataBase = process.env.MONGODB_DATABASE;
 
 global.SALT_KEY = saltKey;
 
 module.exports = {
-  connectionString: `mongodb+srv://${mongodbUsername}:${mongodbPassword}@${mongodbCluster}.xge47gs.mongodb.net/${mongodbDataBase}`,
+  connectionString: `mongodb+srv://${mongodbUsername}:${mongodbPassword}@${mongodbCluster}.${mongodbNet}.mongodb.net/${mongodbDataBase}`,
 };
