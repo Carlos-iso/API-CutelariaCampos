706c9800b8ae80139f5ff969142d69e0df8f2ec8
Carlos-iso <carlosbalak2002@gmail.com>
2023-04-11 09:41:46 -0300

    Update cors

--- src/app/app.js
+++ src/app/app.js
@@ -31,7 +31,7 @@ const urlCadastro = process.env.URL_CADASTRO;
 
 // Habilita O CORS
 app.use(function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', urlHome, urlLogin, urlCadastro);
+	res.header('Access-Control-Allow-Origin', urlCadastro);
 	res.header('Access-Control-Allow-Headers', '*Origen, X-Requested-With, Content-Type, Accept, x-access-token');
 	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 	next();
